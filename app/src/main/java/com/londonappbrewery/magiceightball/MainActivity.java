fce31bd108ac845a1bea01d4e76b52d73440c2af
SimonVanL <simon.vanlombergen.y9313@student.hogent.be>
2017-08-09 18:21:45 +0200

    Finalising 8Ball app

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,12 +2,30 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView ball;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ball = (ImageView) findViewById(R.id.image_eightBall);
+        final int[] images = {R.drawable.ball1, R.drawable.ball2, R.drawable.ball3, R.drawable.ball4, R.drawable.ball5};
+        Button myButton = (Button) findViewById(R.id.button);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random r = new Random();
+                ball.setImageResource(images[r.nextInt(5)]);
+            }
+        });
     }
 }
